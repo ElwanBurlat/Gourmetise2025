a648b229eb7a4b3a33d3e5b4d653a681d7b276d8
ghazli <rostomghazli64@gmail.com>
2026-03-13 08:24:23 +0100

    bouton generation

--- GourmetiseVue/src/router/index.js
+++ GourmetiseVue/src/router/index.js
@@ -2,6 +2,7 @@ import {createRouter, createWebHistory} from 'vue-router'
 import Index from "@/views/index.vue";
 import ContestParams from '@/views/ContestParam.vue';
 import signIn from '@/views/sign-in.vue';
+import results from '@/views/results.vue';
 import CreateConcours from '@/views/ContestParam/CreateConcours.vue';
 import SeeConcours from '@/views/ContestParam/SeeConcours.vue';
 
@@ -22,6 +23,11 @@ const router = createRouter({
             path: '/sign-in',
             name: 'sign-in',
             component: signIn,
+        },
+        {
+            path: '/results',
+            name: 'results',
+            component: results,
         },
         {
             path: '/CreateConcours',
---
+++ GourmetiseVue/src/views/results.vue
@@ -0,0 +1,98 @@
+<template>
+  <div class="resultats-container">
+    <h1 class="titre">Résultat du concours</h1>
+
+    <table class="table-resultats">
+      <thead>
+        <tr>
+          <th>Boulangerie</th>
+          <th>Accueil</th>
+          <th>Présentation</th>
+          <th>Qualité des produits</th>
+          <th>Note finale</th>
+        </tr>
+      </thead>
+
+      <tbody>
+         <tr>
+          <td>{{ bakery }}</td>
+          <td>{{ note_w }}</td>
+          <td>{{ note_s }}</td>
+          <td>{{ note_p }}</td>
+          <td>{{ moyenne}}</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+  <v-btn color="primary" @click="fetchGet">Generation concours !</v-btn>
+
+</template>
+
+<script setup>
+import axios from 'axios'
+import { ref } from 'vue'
+const bakery= ref('')
+const note_w = ref('')
+const note_s = ref('')
+const note_p = ref('')
+const moyenne = ref('')
+  async function fetchGet() {
+  try {
+    const  resultatAPI= await axios.get('http://localhost:8000/api/resultat');
+    const bakeryAPI = await axios.get('http://localhost:8000/api/bakery');
+
+    bakery.value = bakeryAPI.data.bakery;
+
+    note_w.value = resultatAPI.data.note_w;
+    note_s.value = resultatAPI.data.note_s;
+    note_p.value = resultatAPI.data.note_p;
+
+    
+
+
+    }
+    catch (error) {
+    console.error(error)
+  }
+}
+
+</script>
+
+<style scoped>
+
+.resultats-container{
+  width: 85%;
+  margin: 50px auto;
+  text-align: center;
+}
+
+.titre{
+  margin-bottom: 30px;
+  font-size: 32px;
+  color: #2c3e50;
+}
+
+.table-resultats{
+  width: 100%;
+  border-collapse: collapse;
+  background: white;
+  box-shadow: 0 5px 15px rgba(0,0,0,0.1);
+}
+
+.table-resultats th{
+  background-color: #2d6fb7;
+  color: white;
+  padding: 14px;
+  font-size: 16px;
+}
+
+.table-resultats td{
+  padding: 12px;
+  border-bottom: 1px solid #ddd;
+}
+
+.table-resultats tr:hover{
+  background-color: #f5f5f5;
+}
+
+</style>
